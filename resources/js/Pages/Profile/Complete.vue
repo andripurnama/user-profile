eed9bc13abdd5969ded6a1d017327d12a14d7a5d
andripurnama <andri.purnama995@hotmail.com>
2024-05-25 19:09:56 +0700

    feat: added complete profile page

---
+++ resources/js/Pages/Profile/Complete.vue
@@ -0,0 +1,26 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import SectionBorder from '@/Components/SectionBorder.vue';
+import UpdateProfileInformationForm from '@/Pages/Profile/Partials/UpdateProfileInformationForm.vue';
+
+</script>
+
+<template>
+    <AppLayout title="Profile">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                Complete Profile
+            </h2>
+        </template>
+
+        <div>
+            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+                <div v-if="$page.props.jetstream.canUpdateProfileInformation">
+                    <UpdateProfileInformationForm :user="$page.props.auth.user" />
+
+                    <SectionBorder />
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
