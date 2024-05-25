40130d4014561f517eac353106b98c72018a70ff
andripurnama <andri.purnama995@hotmail.com>
2024-05-25 18:02:03 +0700

    feat: add link to profile page

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -44,12 +44,16 @@ const logout = () => {
                                 <Link :href="route('dashboard')">
                                     <ApplicationMark class="block h-9 w-auto" />
                                 </Link>
+                                
                             </div>
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
+                                </NavLink>
+                                <NavLink :href="route('profile.show')" :active="route().current('profile.show')">
+                                    Profile
                                 </NavLink>
                             </div>
                         </div>
